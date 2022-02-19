ca70e5cb5c7018001f7dc6719c51a1158314bd56
kinoshita <kinoshitaryuta@gmail.com>
2022-02-19 10:01:27 +0900

    #master 不要なコメント、実装削除

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,19 +4,8 @@ from.import views
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignupView.as_view(), name='signup'),
     path('signup/finish/<token>/', views.SignupFinishView.as_view(), name='signup_finish'),
     path('signup/done/', views.SignupDoneView.as_view(), name='user_create_done'),
-    path('email/change/', views.EmailChangeView.as_view(), name='email_change'),
-    path('email/change/done/', views.EmailChangeDoneView.as_view(), name='email_change_done'),
-    path('email/change/complete/<str:token>/', views.EmailChangeCompleteView.as_view(), name='email_change_complete'),
-    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset/confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('password_change/done/', views.PasswordDoneView.as_view(), name='password_done'),
-    path('update/restaurant/<int:pk>/', views.RestaurantUserUpdateView.as_view(), name='restaurant_update'),
-    path('login/restaurant/', views.RestaurantLoginView.as_view(), name='restaurant_login'),
-    path('signup/restaurant/', views.RestaurantUserCreateView.as_view(), name='restaurant_signup'),
-    path('restaurant/finish/<token>/', views.RestaurantFinishView.as_view(), name='restaurant_finish'),
+
 ]
